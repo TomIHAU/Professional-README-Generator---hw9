12af3d4e9e7907efd6d910ff29467371ea7092f0
TauH <thaywood988@gmail.com>
2021-10-15 14:01:50 +1100

    finished array -licenses, will work on that later

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
   {
     type: "input",
     name: "description",
-    message: "What is your projects title?",
+    message: "Provide a description of the project?",
   },
   {
     type: "input",
@@ -22,12 +22,12 @@ const questions = [
   {
     type: "input",
     name: "Installation",
-    message: "What is your projects title?",
+    message: "How do you install this project?",
   },
   {
     type: "input",
     name: "Usage",
-    message: "What is your projects title?",
+    message: "How do you use this project?",
   },
   {
     type: "input",
@@ -37,17 +37,17 @@ const questions = [
   {
     type: "input",
     name: "Contributing",
-    message: "What is your projects title?",
+    message: "Who contributed to this project?",
   },
   {
     type: "input",
     name: "Tests",
-    message: "What is your projects title?",
+    message: "What tests has the project done?",
   },
   {
     type: "input",
     name: "Questions",
-    message: "What is your projects title?",
+    message: "What outstanding questions do you have?",
   },
 ];
 //gh-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
